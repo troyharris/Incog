82af69cb86f35caba6d7d90bf8e921b895533d52
Troy HARRIS <troy.harris@djt-tech.com>
2013-08-12 12:06:11 -0700

    Initial Commit

---
+++ Incog/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Incog
+//
+//  Created by Troy HARRIS on 8/12/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ IncogTests/IncogTests.m
@@ -0,0 +1,34 @@
+//
+//  IncogTests.m
+//  IncogTests
+//
+//  Created by Troy HARRIS on 8/12/13.
+//  Copyright (c) 2013 Lone Yeti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IncogTests : XCTestCase
+
+@end
+
+@implementation IncogTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
